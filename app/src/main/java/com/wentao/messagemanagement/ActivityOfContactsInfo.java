b7a2d88f2ffc4932cd5e219035992e320fba392c
LinesAlex <1179662449@qq.com>
2017-11-04 22:53:08 +0800

    加入显示联系人信息的功能。

---
+++ app/src/main/java/com/wentao/messagemanagement/ActivityOfContactsInfo.java
@@ -0,0 +1,39 @@
+package com.wentao.messagemanagement;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+/**
+ * Created by Administrator on 2017/11/4.
+ */
+
+public class ActivityOfContactsInfo extends AppCompatActivity {
+    private TextView tv_name;
+    private TextView tv_phone;
+    private TextView tv_email;
+    private Button btn_back;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.contacts_info);
+        tv_name = (TextView) findViewById(R.id.tv_name_info);
+        tv_phone = (TextView) findViewById(R.id.tv_phone_info);
+        tv_email = (TextView) findViewById(R.id.tv_email_info);
+        Intent intent = getIntent();
+        tv_name.setText(intent.getStringExtra("name"));
+        tv_phone.setText("Phone\n    " + intent.getStringExtra("phone").substring(7));
+        tv_email.setText("Email\n    " + intent.getStringExtra("email").substring(7));
+        btn_back = (Button) findViewById(R.id.btn_back);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/wentao/messagemanagement/MainActivity.java
+++ app/src/main/java/com/wentao/messagemanagement/MainActivity.java
@@ -1,13 +1,17 @@
 package com.wentao.messagemanagement;
 
 import android.Manifest;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -36,6 +40,16 @@ public class MainActivity extends AppCompatActivity {
         } else {
             setContactsListView();
         }
+        contactsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, ActivityOfContactsInfo.class);
+                intent.putExtra("name",((TextView) view.findViewById(R.id.tv_name)).getText());
+                intent.putExtra("phone",((TextView) view.findViewById(R.id.tv_phone)).getText());
+                intent.putExtra("email",((TextView) view.findViewById(R.id.tv_email)).getText());
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
